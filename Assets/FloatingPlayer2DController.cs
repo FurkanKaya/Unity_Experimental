f236cf779698a1279b2781479d497502dd083867
Furkan Kaya <furkan.kaya.web@gmail.com>
2016-12-14 22:26:25 +0300

    Controller Experiment v2

--- Assets/FloatingPlayer2DController.cs
+++ Assets/FloatingPlayer2DController.cs
@@ -5,6 +5,8 @@ using UnityStandardAssets.CrossPlatformInput;
 public class FloatingPlayer2DController : MonoBehaviour
 {
 	Rigidbody2D myBody;
+	private bool movingLeft;
+	private bool movingRight;
 	public float moveForce = 5, boostMultiplier = 2;
 
 	void Start () {
@@ -13,8 +15,24 @@ public class FloatingPlayer2DController : MonoBehaviour
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		Vector2 moveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
-		bool isBoosting = CrossPlatformInputManager.GetButton("Boost");
-		myBody.AddForce(moveVec * (isBoosting ? boostMultiplier : 1));
+		//Vector2 moveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
+		//bool isBoosting = CrossPlatformInputManager.GetButton("Boost");
+		//myBody.AddForce(moveVec * (isBoosting ? boostMultiplier : 1));
+
+		bool movingLeft = CrossPlatformInputManager.GetButton("MoveLeft");
+		bool movingRight = CrossPlatformInputManager.GetButton("MoveRight");
+
+		if (movingRight) {
+			myBody.velocity = new Vector3 (moveForce, myBody.velocity.y, 0f);
+		}
+		else if (movingLeft)
+			{
+				myBody.velocity = new Vector3 (-moveForce, myBody.velocity.y, 0f);
+			}
+			else
+			{
+				myBody.velocity = new Vector3 (0f, myBody.velocity.y, 0f);
+			}
+
 	}
 }
