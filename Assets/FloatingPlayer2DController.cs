cb19e1942db80474043e82613bfd9001f20a5c46
Furkan Kaya <furkan.kaya.web@gmail.com>
2016-12-14 14:19:18 +0300

    Controller Experiment v1
    Using Unity's Standard Assets to generate a mobile 2D platformer game
    controller

---
+++ Assets/FloatingPlayer2DController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class FloatingPlayer2DController : MonoBehaviour
+{
+	Rigidbody2D myBody;
+	public float moveForce = 5, boostMultiplier = 2;
+
+	void Start () {
+		myBody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 moveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
+		bool isBoosting = CrossPlatformInputManager.GetButton("Boost");
+		myBody.AddForce(moveVec * (isBoosting ? boostMultiplier : 1));
+	}
+}
